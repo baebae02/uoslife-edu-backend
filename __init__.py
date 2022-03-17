8383fdc9622d00fe2e50857ac0c7155aeab78c4e
Seo Hyeon Bae <bae4614@gmail.com>
2022-03-17 20:14:29 +0900

    feat: init

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # blueprint
+    from .views import views
+    app.register_blueprint(views.bp)
+
+    return app
