47f96393dab2960fbff92c1b8ab27eb3e72df8f7
Seo Hyeon Bae <bae4614@gmail.com>
2022-03-17 23:51:36 +0900

    feat: edit modles

--- rental_products/models.py
+++ rental_products/models.py
@@ -1,27 +1,27 @@
-from flask_sqlalchemy import Model
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy import ForeignKey, String, Column, Unicode, DateTime, Boolean
+from sqlalchemy import ForeignKey, String, Column,DateTime, Boolean
+from sqlalchemy_utils import UUIDType
 from rental_products import db
+import uuid
 
 
 class User(db.Model):
-    id = Column(Unicode, primary_key=True)
+    __table_name__ = 'user'
+    id = Column(UUIDType, primary_key=True, nullable=False, default=uuid.uuid4)
     nickname = Column(String(50), nullable=False)
     is_admin = Column(Boolean, nullable=False)
-    created_at = Column(DateTime(), nullable=False)
+    created_at = Column(DateTime(), nullable=True)
     updated_at = Column(DateTime(), nullable=True)
     deleted_at = Column(DateTime(), nullable=True)
 
 
-class Products(db.Model):
-    id = Column(Unicode, primary_key=True)
+class Product(db.Model):
+    __table_name__ = 'product'
+    id = Column(UUIDType, primary_key=True, nullable=False)
     nickname = Column(String(50), nullable=False)
     category = Column(String(50), nullable=True)
-    admin_id = Column(Unicode, ForeignKey('user.id', ondelete='CASCADE'))
-    student_id = Column(Unicode, ForeignKey('user.id', ondelete='CASCADE'))
-    admin = relationship('User', backref=backref('product_list', order_by=id))
-    student = relationship('User', backref=backref('rental_list', order_by=id))
-    created_at = Column(DateTime(), nullable=False)
+    admin_id = Column(UUIDType, ForeignKey('user.id', ondelete='CASCADE'))
+    student_id = Column(UUIDType, ForeignKey('user.id', ondelete='CASCADE'))
+    created_at = Column(DateTime(), nullable=True)
     updated_at = Column(DateTime(), nullable=True)
     deleted_at = Column(DateTime(), nullable=True)
     borrowed_at = Column(DateTime(), nullable=True)
